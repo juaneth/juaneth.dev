d3d5534d8703f8ecf6714e1f3aa3bc3c4727ecf9
juaneth <68202118+juaneth@users.noreply.github.com>
2024-04-28 18:40:51 +0100

    made vertical scrolling on project page revert to normal flow

--- src/FlowPages/Projects/UniSSH.jsx
+++ src/FlowPages/Projects/UniSSH.jsx
@@ -1,4 +1,4 @@
-import GitHubIcon from "@mui/icons-material/GitHub";
+import { FaDiscord, FaGithub } from "react-icons/fa";
 
 export default function UniSSH() {
   return (
@@ -11,8 +11,9 @@ export default function UniSSH() {
               <h2 className='text-3xl'>A modern SSH Client</h2>
             </div>
 
-            <div className='flex flex-row h-min gap-3 justify-center items-center'>
+            <div className='flex flex-row h-min gap-5 justify-center items-center'>
               <div
+                role='button'
                 onClick={() => {
                   open(
                     "https://github.com/uniSSH-Team/uniSSH-releases",
@@ -20,10 +21,16 @@ export default function UniSSH() {
                   );
                 }}
               >
-                <GitHubIcon
-                  className='text-white/75 hover:text-white'
-                  fontSize='large'
-                ></GitHubIcon>
+                <FaGithub className='text-4xl hover:text-white transition-all'></FaGithub>
+              </div>
+
+              <div
+                role='button'
+                onClick={() => {
+                  open("https://discord.gg/GNweWy8YWc", "_blank");
+                }}
+              >
+                <FaDiscord className='text-4xl hover:text-white transition-all'></FaDiscord>
               </div>
             </div>
           </div>
@@ -32,6 +39,13 @@ export default function UniSSH() {
 
           <div className='px-6 py-5 bg-slate-500/15 rounded-md bordered-all'>
             <h1 className='text-3xl font-semibold text-white'>Features</h1>
+
+            <div className='grid grid-cols-8 gap-4'>
+              <div className='bg-white col-span-3'>
+                <h1>Clear design language</h1>
+              </div>
+              <div className='bg-white'></div>
+            </div>
           </div>
         </div>
       </div>
