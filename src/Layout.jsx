8be601ec28cd9fb8350f105b112b3035bd6b3989
juaneth <68202118+juaneth@users.noreply.github.com>
2024-04-30 23:49:08 +0100

    Fixed typo

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function Layout() {
   return (
