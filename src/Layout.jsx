ce3cd62ad9ffa7ad22d19883df5b4099e2e09184
juaneth <68202118+juaneth@users.noreply.github.com>
2024-04-30 23:46:47 +0100

    Added speed insights

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Layout() {
   return (
     <>
       <Analytics></Analytics>
+      <SpeedInsights></SpeedInsights>
       <div className='flex flex-col min-h-screen'>
         <div className='grow'>
           <Outlet />
