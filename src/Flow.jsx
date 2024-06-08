e48626c6d778dd71cbfd54d57c5a82e7af7c7953
juaneth <68202118+juaneth@users.noreply.github.com>
2024-06-08 22:47:32 +0100

    added bloom effect

--- src/Flow.jsx
+++ src/Flow.jsx
@@ -239,6 +239,7 @@ export default function Flow() {
         <Socials></Socials>
       </div>
       <div className='bg fixed top-0 left-0 h-screen w-screen -z-10'></div>
+      <div className='bloom fixed top-0 left-0 h-screen w-screen -z-10'></div>
     </>
   );
 }
--- src/FlowPages/Home.jsx
+++ src/FlowPages/Home.jsx
@@ -7,13 +7,13 @@ export default function Home() {
   return (
     <>
       <div className='panel h-screen min-h-screen max-h-screen'>
-        <div className='flex flex-col gap-8 justify-center items-center h-full'>
-          <h1 className='text-9xl font-bold text-white tracking-tight'>
+        <div className='flex flex-col sm:gap-8 gap-4 justify-center items-center text-center h-full'>
+          <h1 className='sm:text-9xl text-6xl font-bold text-white tracking-tight'>
             juaneth.dev
           </h1>
-          <p className='text-5xl'>Desktop + Web developer</p>
+          <p className='sm:text-5xl text-3xl'>Desktop + Web developer</p>
 
-          <div className='text-warning text-2xl opacity-75 flex flex-row items-center gap-3'>
+          <div className='text-warning sm:text-2xl text-xl opacity-75 flex flex-row items-center gap-3'>
             Best viewed on desktop <IoIosWarning></IoIosWarning>
           </div>
         </div>
